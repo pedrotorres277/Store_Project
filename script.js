acb65dc91c10a813a4f373f4ef2d38942735c311
Pedro Torres <pedrolucast.2727@gmail.com>
2024-07-24 17:59:24 -0300

    js in working

--- script.js
+++ script.js
@@ -36,9 +36,43 @@ menu.addEventListener("click", function (e) {
 });
 
 function addToCart(name, price) {
-  cart.push({
-    name,
-    price,
-    quantity: 1,
+  const existingItem = cart.find((item) => item.name === name);
+
+  if (existingItem) {
+    existingItem.quantity += 1;
+  } else {
+    cart.push({
+      name,
+      price,
+      quantity: 1,
+    });
+  }
+
+  updateCartModal();
+}
+
+function updateCartModal() {
+  let total = 0;
+
+  cart.forEach((item) => {
+    const cartItemElement = document.createElement("div");
+
+    cartItemElement.innerHTML = `
+    <div>
+      <div>
+        <p>${item.name}</p>
+        <p>${item.quantity}</p>
+        <p>R$ ${item.price}</p>
+      </div>
+
+      <div>
+        <button>
+        Remover
+        </button> 
+      </div>
+    </div>
+    `;
+
+    cartItemsContainer.appendChild(cartItemElement);
   });
 }
